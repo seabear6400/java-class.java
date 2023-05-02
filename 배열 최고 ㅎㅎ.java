2aecf05edefd4259f066fb9e730d27d5a30e9ebf
김성우 <126859882+seabear6400@users.noreply.github.com>
2023-05-02 17:58:32 +0900

    Create 배열 최고 ㅎㅎ.java

---
+++ 배열 최고 ㅎㅎ.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ehsdv {
+
+	public static void main(String[] args) {
+		
+//		int[] 배열 = new int[100];
+//		int 배열2[] = new int[100];
+//		System.out.println(배열[18]);
+		
+		int[] 배열 = new int[5];
+		int dut = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		while(dut<배열.length) {
+			
+			System.out.println("값을 입력하시오!");
+			
+			배열[dut] = sc.nextInt();
+			dut++;
+		}
+		for(int 영원= 0;영원<배열.length;영원++) {
+		
+		System.out.println(배열[영원]);
+		}
+	}
+
+}
