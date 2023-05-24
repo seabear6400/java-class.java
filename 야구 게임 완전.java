72f1dd87132d1e78b5570d861465296e77e45be5
김성우 <126859882+seabear6400@users.noreply.github.com>
2023-05-24 18:01:03 +0900

    Create 야구 게임 완전.java

---
+++ 야구 게임 완전.java
@@ -0,0 +1,66 @@
+package asdf;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class aasdg {
+
+	public static void main(String[] args) {
+		  Random rd = new Random();
+	       
+	        int 정 = rd.nextInt(9)+1;
+	        int 연 = rd.nextInt(9)+1;
+	        int 택 = rd.nextInt(9)+1;
+	       
+	        while(정==연||연==택||택==정) {
+	            정 = rd.nextInt(9)+1;
+	            연 = rd.nextInt(9)+1;
+	            택 = rd.nextInt(9)+1;
+	        }
+	       
+//	        System.out.println(정);
+//	        System.out.println(연);
+//	        System.out.println(택);
+	       
+	        Scanner sc = new Scanner(System.in);
+	        // 사용자가 숫자 3개를 입력
+	        int s=0;
+	        int b=0;
+	        
+	        
+	        while(s!=3) {
+		        System.out.print("숫자 3개 입력해봐");
+		        int jyt = sc.nextInt();
+		        if(jyt>999) {
+		        	
+		            System.out.println("응 다시해~");
+		            
+		        } else {
+		        	s = 0;
+		        	b = 0;
+		        	
+		            int t = jyt % 10; // 3
+		            jyt = jyt / 10; // 12
+		            int y = jyt % 10; // 2
+		            jyt = jyt / 10; // 1
+		            int j = jyt % 10; // 1
+		           
+		            // 스트라이크 인지 볼인지 판단
+		            // 컴퓨터 : 정 연 택
+		            // 입력한 : j  y t
+		            if(정==j) s++;
+		            if(연==y) s++;
+		            if(택==t) s++;
+		            
+		            if(j==연 || j==택) b++;
+		            if(y==정 || y==택) b++;
+		            if(t==정 || t==연) b++;
+
+		        }
+		        System.out.println(s+"Strike "+b+"Ball!");
+		       }
+	        System.out.println("축하합니다.");
+
+		}
+
+	}
